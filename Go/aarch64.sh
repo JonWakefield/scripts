8496a29cf3e8179d011ddd5d752871dff0f7abbd
JonWakefield <jonwakefield.mi@gmail.com>
2024-12-18 19:11:01 -0700

    added admin support for golang install

--- Go/aarch64.sh
+++ Go/aarch64.sh
@@ -11,12 +11,15 @@ DOWNLOAD_URL="https://go.dev/dl/go${GO_VERSION}.linux-arm64.tar.gz"
 read -p "Remove Previous Go version if exists (y/n)? " removePrev
 if [ "$removePrev" = "y" ]; then
 	echo "Removing Previous Go version"
-	rm -rf /usr/local/go
+	sudo rm -rf /usr/local/go
 fi
+
+sudo apt update
+sudo apt install wget
 
 wget -P /usr/local $DOWNLOAD_URL
 
-tar -C /usr/local -xzf go$GO_VERSION.linux-arm64.tar.gz
+sudo tar -C /usr/local -xzf go$GO_VERSION.linux-arm64.tar.gz
 
 echo "successfully installed Golang version ${GO_VERSION}"
 echo "Add binaries to path. 'export PATH=$PATH:/usr/local/go/bin'"
