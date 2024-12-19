ae91ba99b86eb6770c8874fe2adfa03e32d8189a
JonWakefield <jonwakefield.mi@gmail.com>
2024-12-18 19:22:45 -0700

    Updated golang shell install

--- Go/aarch64.sh
+++ Go/aarch64.sh
@@ -4,6 +4,8 @@
 # Golang install script, intended for linux, arm64 based devices
 #---------------------------------------------------
 
+set -e
+
 GO_VERSION="1.23.4"
 DOWNLOAD_URL="https://go.dev/dl/go${GO_VERSION}.linux-arm64.tar.gz"
 
@@ -17,9 +19,28 @@ fi
 sudo apt update
 sudo apt install wget
 
-wget -P /usr/local $DOWNLOAD_URL
+# download
+wget -q $DOWNLOAD_URL -O /tmp/go${GO_VERSION}.tar.gz
 
-sudo tar -C /usr/local -xzf go$GO_VERSION.linux-arm64.tar.gz
+# extract tarball
+sudo tar -C /usr/local -xzf /tmp/go${GO_VERSION}.tar.gz
 
+# cleanup 
+sudo rm /tmp/go${GO_VERSION}.tar.gz
 echo "successfully installed Golang version ${GO_VERSION}"
-echo "Add binaries to path. 'export PATH=$PATH:/usr/local/go/bin'"
+
+echo "Adding Go to User Path"
+PROFILE_FILE="~/.bashrc" # if using bash
+if [[ $SHELL == *"zsh"* ]]; then
+	PROFILE_FILE="~/.zshrc"
+fi
+
+if ! grep -q "export PATH=\$PATH:/usr/local/go/bin" ${PROFILE_FILE}; then
+	echo "export PATH=\$PATH:/usr/local/go/bin" >> ${PROFILE_FILE}
+fi
+
+# load updated path
+eval "$(tail -n 1 ${PROFILE_FILE})"
+
+
+echo "Go ${GO_VERSION} installation completed successfully"
